Fix near and far scaling

main.py
@@ -97,9 +97,10 @@ def train(**conf):
         conf["scale"], 
         nerfdataset.t_min, nerfdataset.t_max,
         N=30)
+    nerfdataset_test = nerfdataset
     dataloader = Dataloader(dataloader_key, nerfdataset, conf["batch_size"])
 
-    gt_ids = jnp.array([0, 5, 10, 15, 20, 25, 29], dtype=jnp.int32)
+    gt_ids = jnp.array([0, 15, 20, 29], dtype=jnp.int32)
     ground_truth_images = jax.vmap(lambda i: nerfdataset_test.images[i]) (gt_ids)
 
     cameras = []

nerf/datasets/blender.py
@@ -69,7 +69,7 @@ class BlenderDataset(Dataset):
         self.W = np.array(self.W)
         self.f = np.array(self.f)
 
-        self.cameras = jax.vmap(lambda x, h, w, f: PinholeCamera(f, h, w, x))(
+        self.cameras = jax.vmap(lambda x, h, w, f: PinholeCamera(f, h, w, x, 0.01, 3))(
             self.poses, self.H, self.W, self.f
         )
 